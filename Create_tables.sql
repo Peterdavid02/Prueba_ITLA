6417d6476a05377ac07daf45a168d823fea016db
Peterdavid02 <50086745+Peterdavid02@users.noreply.github.com>
2023-02-07 14:39:53 -0400

    habia una tabla con un campo que tenia una tilde

--- Create_tables.sql
+++ Create_tables.sql
@@ -2,14 +2,14 @@
 IF ( NOT EXISTS (SELECT * 
                  FROM INFORMATION_SCHEMA.TABLES 
                  WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'Tipo_de_identificaci髇'))
+                 AND  TABLE_NAME = 'Tipo_de_identificaci贸n'))
 	BEGIN
-	CREATE TABLE Tipo_de_identificaci髇 (
+	CREATE TABLE Tipo_de_identificaci贸n (
 		id INT PRIMARY KEY IDENTITY(1,1),
 		type VARCHAR(50) NOT NULL
 	); 
 	END
-	ELSE PRINT('LA TABLA Tipo_de_identificaci髇 YA EXISTE ')
+	ELSE PRINT('LA TABLA Tipo_de_identificaci贸n YA EXISTE ')
 
 IF (NOT EXISTS (SELECT * 
                  FROM INFORMATION_SCHEMA.TABLES 
@@ -20,10 +20,10 @@ IF (NOT EXISTS (SELECT *
 					id INT PRIMARY KEY IDENTITY(1,1),
 					Nombres VARCHAR(50) NOT NULL,
 					Apellidos VARCHAR(50) NOT NULL,
-					Identificaci髇 VARCHAR(50) NOT NULL,
+					Identificacion VARCHAR(50) NOT NULL,
 					Fecha_de_Nacimiento DATE NOT NULL,
 					tipo_id  int NOT NULL,
-					FOREIGN KEY (tipo_id) REFERENCES Tipo_de_identificaci髇(id)
+					FOREIGN KEY (tipo_id) REFERENCES Tipo_de_identificaci贸n(id)
 					);
 					END
 				ELSE PRINT('LA TABLA persona YA EXISTE ')
